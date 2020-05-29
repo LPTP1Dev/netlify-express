59929482ad0d79024c4afb61c83246dfe7515125
LPTP1Dev <51876648+LPTP1Dev@users.noreply.github.com>
2020-05-29 23:07:49 +0200

    Updated bug

--- express/server.js
+++ express/server.js
@@ -5,14 +5,14 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.get('/', (req, res) => {
+
+app.get('/', (req, res) => {
   res.redirect("https://bracketbot.ga")
 });
-router.get('/invite', (req, res) => res.redirect("https://discord.com/api/oauth2/authorize?client_id=606197308635480093&permissions=8&scope=bot"));
-router.get('/support', (req, res) => res.redirect("https://discord.gg/7e9kcaX"));
+app.get('/invite', (req, res) => res.redirect("https://discord.com/api/oauth2/authorize?client_id=606197308635480093&permissions=8&scope=bot"));
+app.get('/support', (req, res) => res.redirect("https://discord.gg/7e9kcaX"));
 
-router.get("*", (req, res) {
+app.get("*", (req, res) {
 res.redirect("/")
 })
 app.use('/.netlify/functions/server', router);  // path must route to lambda
