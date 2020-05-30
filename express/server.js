312a8e6888abb9d821fbc2d8e2601f7a7b796a9b
LPTP1Dev <51876648+LPTP1Dev@users.noreply.github.com>
2020-05-30 21:07:42 +0200

    Update

--- express/server.js
+++ express/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 
 app.get('/', (req, res) => {
-  res.redirect("https://bracketbot.ga")
+  res.sendFile(__dirname+"/../index.html")
 });
 app.get('/invite', (req, res) => res.redirect("https://discord.com/api/oauth2/authorize?client_id=606197308635480093&permissions=8&scope=bot"));
 app.get('/support', (req, res) => res.redirect("https://discord.gg/7e9kcaX"));
@@ -15,7 +15,7 @@ app.get('/support', (req, res) => res.redirect("https://discord.gg/7e9kcaX"));
 app.get("*", (req, res) {
 res.redirect("/")
 })
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', app);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
